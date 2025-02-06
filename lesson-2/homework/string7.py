7eb2a04d906cc22e7a17f6b359a9391125a36e5b
laziz <khushmurodovlaziz@gmail.com>
2025-02-06 21:48:46 +0500

    fixed string7

--- lesson-2/homework/string7.py
+++ lesson-2/homework/string7.py
@@ -1,16 +1,5 @@
 txt = input("txt = ")
 repl = input('replace the word ')
 repl_with = input('replace with ')
-result = ''
-splitted = txt.split(' ')
-contains = False
-for i in range(len(splitted)):
-    if splitted[i]==repl:
-        splitted[i]=repl_with
-        contains = True
-if contains:
-    for x in splitted:
-        result+=x
-        result+=" "
-
+result = txt.replace(repl, repl)
 print(result) 
