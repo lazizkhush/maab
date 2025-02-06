b447e0770296758fd068102e3ae28cebbfb40dee
laziz <khushmurodovlaziz@gmail.com>
2025-02-06 21:46:28 +0500

    changed number2.py

--- lesson-2/homework/number2.py
+++ lesson-2/homework/number2.py
@@ -2,19 +2,8 @@ a = int(input('a = '))
 b = int(input('b = '))
 c = int(input('c = '))
 
-if (a>b and a>c):
-    largest = a
-elif (b>a and b>c):
-    largest = b 
-else:
-    largest = c
-
-if (a>b and c>b):
-    smallest = b
-elif (b>a and c>a):
-    smallest = a 
-else:
-    smallest = c  
+largest = max(a, b, c)
+smallest = min(a, b, c)
 
 print(f"{largest=} ")
 print(f"{smallest=} ")
