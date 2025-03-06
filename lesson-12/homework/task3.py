871bcdfaa71e2f52160fc482ade8f044b3fe5b9f
laziz <khushmurodovlaziz@gmail.com>
2025-03-06 14:39:02 +0500

    lessin-12

--- lesson-12/homework/task3.py
+++ lesson-12/homework/task3.py
@@ -1,45 +1,33 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import json
 import time
+import json
 
-def scrape_laptops():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://www.demoblaze.com/")
-    time.sleep(3)
-    
-    laptops = []
-    
-    # Click on the Laptops category
-    driver.find_element(By.LINK_TEXT, "Laptops").click()
-    time.sleep(3)
-    
-    while True:
-        items = driver.find_elements(By.CLASS_NAME, "card")
-        for item in items:
-            name = item.find_element(By.CLASS_NAME, "card-title").text
-            price = item.find_element(By.CLASS_NAME, "card-price").text.replace("$", "")
-            description = item.find_element(By.CLASS_NAME, "card-text").text
-            laptops.append({"name": name, "price": price, "description": description})
-        
-        # Try clicking the next page button
-        try:
-            next_button = driver.find_element(By.LINK_TEXT, "Next")
-            driver.execute_script("arguments[0].click();", next_button)
-            time.sleep(3)
-        except:
-            break  # If no Next button, exit loop
-    
-    driver.quit()
-    
-    # Save data to JSON file
-    with open("laptops.json", "w", encoding="utf-8") as f:
-        json.dump(laptops, f, indent=4)
-    
-    print("Data saved to laptops.json")
+chrome_options = Options()
+chrome_options.add_experimental_option('detach', True)
 
-if __name__ == "__main__":
-    scrape_laptops()
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get('https://www.demoblaze.com/#')
+btn_laptops = driver.find_element(By.LINK_TEXT, 'Laptops')
+btn_laptops.click()
+
+time.sleep(3)
+btn_next = driver.find_element(By.ID, 'next2')
+btn_next.click()
+
+time.sleep(2)
+info = []
+
+items = driver.find_elements(By.CLASS_NAME, 'card')
+for item in items:
+    title = item.find_element(By.CLASS_NAME, 'card-title').text
+    price = item.find_element(By.TAG_NAME, 'h5').text
+    desc = item.find_element(By.ID, 'article').text
+    info.append({'name':title, 'price':price, 'description':desc})
+
+info_json = json.dumps(info, indent=4)
+print(info_json)
+driver.quit()
